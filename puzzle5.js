29a21e1f6dd3eb38bfd04f1e07049c9d5a4d7b75
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-01-23 13:03:18 -0800

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,34 @@
+right
+right
+var move = getColor
+	if (getColor==“blue”) {
+	down
+	right
+
+}
+	if (getColor==“pink”) {
+	up
+	right
+}
+var move = getColor
+	if (getColor==“blue”) {
+	down
+	right
+
+}
+	if (getColor==“pink”) {
+	up
+	right
+}
+var move = getColor
+	if (getColor==“blue”) {
+	down
+	right
+
+}
+	if (getColor==“pink”) {
+	up
+	right
+}
+right
+right
