602e5a066baa9c2226cde79c0cb882a0ac2a0bb8
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-01-23 13:04:16 -0800

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,26 @@
+right
+var move = getColor ()
+	if (getColor == “pink”) {
+	up
+	up
+	up
+	getColor ()
+		if (getColor == “pink”) {
+		left
+		up
+	} 
+		if (getColor == “blue”) {
+		right
+		up
+	} 
+}
+	if (getColor == “blue”) {
+	down
+	down
+	down
+	getColor ()
+		if (getColor == “pink”) {
+		left
+		down
+	} 
+}
