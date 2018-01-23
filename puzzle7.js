3baf0c6c7b69ab73e553b3abb3bbf48dbb3d6214
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-01-23 13:05:12 -0800

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,23 @@
+while (getColor != “pink”) {
+	down
+}
+right
+right
+while (getColor != “pink”) {
+	up
+}
+right right
+while (getColor != “pink”) {
+	down
+}
+right
+right
+while (getColor != “pink”) {
+	up
+}
+right
+right
+while (getColor != “pink”) {
+	down
+}
+right
