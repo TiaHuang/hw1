9daed79fd0f6789d7e3824574c849c1b134f8041
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-01-23 13:05:39 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,30 @@
+var steps = 0
+while (steps < 8) {
+	down
+	steps = steps +1
+}
+right
+right
+while (steps < 7) {
+	up
+	steps = steps +1
+}
+right
+right
+while (steps < 7) {
+	down
+	steps = steps +1
+}
+right
+right
+while (steps < 7) {
+	up
+	steps = steps +1
+}
+right
+right
+while (steps < 7) {
+	down
+	steps = steps +1
+}
+right
