381ed20d0d852527d1fbaf9a831fa61a7ca21591
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-01 01:52:22 -0800

    Update puzzle3.js

--- puzzle3.js
+++ puzzle3.js
@@ -1,12 +1,12 @@
-down
-down
-var colorSeen=getColor ()
-down
-down
-down
-right
-right
-setColor (colorSeen)
-right
-right
-up
+down();
+down();
+var colorSeen=getColor();
+down();
+down();
+down();
+right();
+right();
+setColor (colorSeen);
+right();
+right();
+up();
