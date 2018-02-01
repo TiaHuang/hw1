8da1a54da5d7d87f7ca7d50253123fae904e77fb
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-01 01:54:14 -0800

    Update puzzle8.js

--- puzzle8.js
+++ puzzle8.js
@@ -1,30 +1,38 @@
 var steps = 0
 while (steps < 8) {
-	down
-	steps = steps +1
+    down();
+    steps = steps +1
 }
-right
-right
+right();
+right();
+
+var steps = 0
 while (steps < 7) {
-	up
-	steps = steps +1
+    up();
+    steps = steps +1
 }
-right
-right
+right();
+right();
+
+var steps = 0
 while (steps < 7) {
-	down
-	steps = steps +1
+    down();
+    steps = steps +1
 }
-right
-right
+right();
+right();
+
+var steps = 0
 while (steps < 7) {
-	up
-	steps = steps +1
+    up();
+    steps = steps +1
 }
-right
-right
+right();
+right();
+
+var steps = 0
 while (steps < 7) {
-	down
-	steps = steps +1
+    down();
+    steps = steps +1
 }
-right
+right();
