a8e1363677b47d468dbdcb36a850b581c46feece
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-01 01:52:45 -0800

    Update puzzle4.js

--- puzzle4.js
+++ puzzle4.js
@@ -1,17 +1,17 @@
-down
-down
-down
-down
-right
-var colorSeen = getColor
-down
-setColor (colorSeen)
-right
-right
-up
-right
-var colorSeen = getColor
-down
-setColor (colorSeen)
-right
-right
+right();
+down();
+down();
+down();
+down();
+var colorSeen = getColor();
+down();
+setColor (colorSeen);
+right();
+right();
+up();
+right();
+var secondColorSeen = getColor();
+down();
+setColor (secondColorSeen);
+right();
+right();
