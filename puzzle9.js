2e5acc893032b8bb8385bd5b03245967346dce4f
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-01 01:55:08 -0800

    Create puzzle9.js

---
+++ puzzle9.js
@@ -0,0 +1,119 @@
+function open_rectangle1() {
+  var steps = 0
+  while (steps < 10) {
+      down();
+        setColor("red");
+      steps = steps +1
+  }
+
+  while (steps < 20) {
+      right();
+        setColor("red");
+      steps = steps +1
+  }
+
+  while (steps < 30) {
+      up();
+        setColor("red");
+      steps = steps +1
+  }
+
+  while (steps < 40) {
+      left();
+        setColor("red");
+      steps = steps +1
+  }
+}
+
+function open_rectangle2() {
+  var steps = 0
+  while (steps < 9) {
+    down();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 17) {
+    right();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 25) {
+    up();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 32) {
+    left();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 39) {
+    down();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 45) {
+    right();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 51) {
+    up();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 56) {
+    left();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 61) {
+    down();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 65) {
+    right();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 69) {
+    up();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 72) {
+    left();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 75) {
+    down();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 77) {
+    right();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 79) {
+    up();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 80) {
+    left();
+       setColor("blue");
+    steps = steps +1
+  }
+  while (steps < 81) {
+    down();
+       setColor("blue");
+    steps = steps +1
+  }
+}
+
+open_rectangle1();
+right();
+open_rectangle2();
